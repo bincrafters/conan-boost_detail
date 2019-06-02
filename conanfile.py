c2007e4437369056767fafc2230b69ad5ed63019
Rene Rivera <grafikrobot@gmail.com>
2019-06-02 01:11:25 -0500

    Boost 1.70.0 [skip ci]

--- conanfile.py
+++ conanfile.py
@@ -4,18 +4,9 @@
 from conans import python_requires
 
 
-base = python_requires("boost_base/1.69.0@bincrafters/testing")
+base = python_requires("boost_base/2.0.0@bincrafters/testing")
+
 
 class BoostDetailConan(base.BoostBaseConan):
     name = "boost_detail"
-    version = "1.69.0"
-    url = "https://github.com/bincrafters/conan-boost_detail"
-    lib_short_names = ["detail"]
-    header_only_libs = ["detail"]
-    b2_requires = [
-        "boost_config",
-        "boost_core",
-        "boost_preprocessor",
-        "boost_static_assert",
-        "boost_type_traits"
-    ]
+    version = "1.70.0"
